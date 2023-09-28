ff83df1f2be821e54270ff783669e45b22efa329
martinlarosa27 <martinlarosa27@gmail.com>
2023-09-28 16:47:18 -0300

    cambios

--- 1-Introduccion/parte1.py
+++
@@ -1,9 +0,0 @@
-print("Hola Mundo")
-
-x = 15
-x = 19.99
-
-name = "Luna"
-saludo = f"Hola {name}"
-
-pedido = int(input("Ingrese su edad: "))
--- 1-Introduccion/parte4.py
+++
@@ -1,8 +0,0 @@
-def myFunction(a, b):
-    print(a + b)
-    
-def myFunctionDos(a, b=20):
-    print(a + b)
-
-myFunction(a=20, b=10)
-myFunctionDos(a=20)
