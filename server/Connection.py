ce3a41479b5ff5ea044b30a1df6e941d7c1abc59
martinlarosa27 <martinlarosa27@gmail.com>
2023-10-05 14:40:10 -0300

    varios

--- server/Connection.py
+++ server/Connection.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+
+# Accede a lasa variables de entorno:
+load_dotenv()
 
 # Evalua el nombre del modulo:
 app = Flask(__name__)
 CORS(app)
 
 # Crear conexión as DB:
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "root"
-app.config["MYSQL_DB"] = "microblog"
+app.config["MYSQL_HOST"] = os.getenv("DB_HOST")
+app.config["MYSQL_USER"] = os.getenv("DB_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("DB_PASS")
+app.config["MYSQL_DB"] = os.getenv("DB_NAME")
 mysql = MySQL(app)
